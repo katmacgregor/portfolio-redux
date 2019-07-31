9449e254aa6515293867fcb88beb546205ef0d47
Katrina MacGregor <macgregor@apple.com>
2019-07-31 15:57:19 -0700

    Update colors

--- port3/src/components/browse/browse.js
+++ port3/src/components/browse/browse.js
@@ -92,7 +92,7 @@ class Browse extends React.Component {
           <div className="about content-container fill-screen">
             <div className="content">
               <p>I made my debut as an artist with "Thanksgiving is Yummy" (Crayola on loose leaf 8.5" by 11"), featured in the kindergarten parent newsletter. This marked the dawn of my creative exploits, which evenutally lead to receiving my BA in Painting and Design from USC's Roski School of Fine Arts.</p>
-              <p>My career hopped in a natural progression from design into UI &amp; UX, and I eventually built up my rapid prototyping skillset to lead to my current position in web development as a Software Engineer at Apple.</p>
+              <p>My career hopped in a natural progression from design into UI &amp; UX, and I eventually built up my rapid prototyping skillset to lead to my current position in web development as a UX/UI Engineer at Apple.</p>
               <p>As a developer, I translate my passion for art and design into responsive code and interactive web elements. Daily languages include Javascript, HTML, CSS, and frameworks/libraries including React, Node, MongoDB, and Sass.</p>
               <p> When I'm not tinkering with code, you can find me doting on my pet rabbit, or wandering the mountains in pursuit of the Milky Way.</p>
             </div>
