2e5968505b1042d4d8d6e322dde9ae88edd702d6
Katrina MacGregor <macgregor@apple.com>
2019-08-03 11:10:19 -0700

    Reorganize file structure

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# portfolio-redux
-updated portfolio website & redesign
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+Get up and running: `npm install` then `npm start`
+
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+
+### `npm test`
+### `npm run build`
+### `npm run eject`
---
+++ build/precache-manifest.0bd23f7a48bb0adb92a52e13de40f26f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2386397ed0668b303b0644ed5d7b0e59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5085b662eb4f8f080419",
+    "url": "/static/css/main.889ddc5d.chunk.css"
+  },
+  {
+    "revision": "f3f802b2a3b741662dcc",
+    "url": "/static/js/2.c71ece08.chunk.js"
+  },
+  {
+    "revision": "5085b662eb4f8f080419",
+    "url": "/static/js/main.ed817c28.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e3b46447ec55229d271d9b626079b42b",
+    "url": "/static/media/King-Basil-Lite.e3b46447.woff"
+  },
+  {
+    "revision": "76810700f336be17aca4e46f61ab8f31",
+    "url": "/static/media/Muli-Regular.76810700.woff"
+  },
+  {
+    "revision": "94886198b0a613293e3bf77a2346c6d7",
+    "url": "/static/media/arrow.94886198.svg"
+  },
+  {
+    "revision": "93ffb3570baffc4e8d01c7993ecaf9f9",
+    "url": "/static/media/icons_connect.93ffb357.svg"
+  }
+]);
