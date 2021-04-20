4c18bcce69f262eb3e111d988345a3724a364569
Katrina MacGregor <macgregor@apple.com>
2021-04-20 15:34:06 -0700

    Description update

--- src/components/browse/browse.js
+++ src/components/browse/browse.js
@@ -92,10 +92,10 @@ class Browse extends React.Component {
 
           <div className="about content-container fill-screen">
             <div className="content">
-              <p>I made my debut as an artist with "Thanksgiving is Yummy" (Crayola on loose leaf 8.5" by 11"), featured in the kindergarten parent newsletter. This first piece was just one of many, and culminated in getting my BA in Painting and Design at USC's Roski School of Fine Arts.</p>
-              <p>My zeal for design transitioned into UI &amp; UX, and rapid protopying quickly grew into full-fledged web development. Today, I'm a UX Engineer at Apple.</p>
-              <p>As a developer, I love puzzle-solving, and take a mobile-first, responsive approach in design and code. I get most excited by bringing pages to life with dynamic, performant, and configurable interactions. Daily languages include Javascript, HTML, CSS, and frameworks/libraries including React, Node, MongoDB, and Sass.</p>
-              <p>When I'm not tinkering with code, you can find me lounging with my pet rabbit, or wandering the mountains in pursuit of the Milky Way.</p>
+              <p>I made my debut as an artist with "Thanksgiving is Yummy" (Crayola on loose leaf 8.5" by 11"), featured in the kindergarten parent newsletter. Art and I made it official at USC's Roski School of Fine Arts with a BA in Painting and Design.</p>
+              <p>My zeal for design expanded to include a dedication to user experience, and I taught myself code to bring those experiences to life. I dedicated many years to web app development and storytelling, leading the design translation and front-end development efforts on sites such as <a href="https://www.starwars.com/">StarWars.com</a> and <a href="https://www.disney.com/">Disney.com</a>. Today, I employ a combination of creative strategy and development prototyping as a UX Engineer at Apple. </p>
+              <p>I love puzzle-solving, and take a mobile-first, responsive approach in design and code. I am passionate about rapidly prototyping and iterating user experiences, and find joy in microinteractions. My most utilized languages include Javascript, HTML, CSS, and frameworks/libraries including React, Node, MongoDB, Sass, and SwiftUI.</p>
+              <p>When I'm not tinkering with code, you can find me trying to pick up a new outdoor hobby, or in pursuit of the tastiest bowl of ramen.</p>
             </div>
           </div>
         </div>
